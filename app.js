b421e6383b58b444b4479e51edf292187c1b6d49
Nguyen Xuan Anh Duy (CME.MSA) <DuyNXA@fsoft.com.vn>
2018-01-05 09:33:02 +0700

    Conplete create profile

--- app.js
+++ app.js
@@ -10,11 +10,11 @@
 
   });
   // controller to get profile data
-  app.controller('MainPage', function($scope, $http){
+  app.controller('ProfileController', function($scope, $http){
     var settings = {
       method: 'GET',
       url: 'http://test.fastget.net/api/profiles/4',
-      headers:{"authorization": "Basic ZGRnZ2VlOkZnMTAwITIzNA=="},
+      headers:{"authorization": "Basic ZHV5bnhhOkZnMTAwITIzNDU="},
       async: true,
       crossDomain: true
     }
@@ -23,12 +23,40 @@
       })
   });
 
-  // define directive for Register
-  app.directive('appRegister', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'partials/register.html'
-    };
+  // controller for profile Register
+  app.controller('ProfileCreation', function ($scope, $http) {
+    $scope.language = "en"
+    var settings = {
+      method: 'POST',
+      url: 'http://test.fastget.net/api/profiles/',
+      data: {},
+      headers:{
+        "x-csrftoken": "csrf",
+        "content-type": "application/json",
+        "authorization": "Basic ZmFzdGdldDpGZHNhJDMyMQ=="
+      },
+      async: true,
+      crossDomain: true
+    }
+
+    $scope.createProfile = function (){
+      var profileObject = {
+        "username":$scope.phone,
+        "first_name":$scope.first_name,
+      	"last_name":$scope.last_name,
+      	"phone":$scope.phone,
+      	"password":$scope.password,
+      	"address":$scope.address,
+      	"email":$scope.email,
+      	"language":$scope.language
+      };
+      settings.data = Object.assign(settings.data,profileObject);
+      $http(settings).then(function(data){
+          $scope.returnData = data.data;
+      });
+    }
   });
+
+  // define directive for Register
 
 })();
