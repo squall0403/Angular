e63217a73d765ce0a9302c406bbfa45afcc2564c
squall0403 <nguyenduy574@gmail.com>
2018-01-08 21:00:15 +0700

    Adding languages

--- app.js
+++ app.js
@@ -2,73 +2,41 @@
 
   // Define module
   var app = angular.module('app', [
-
-    'ionic'
+    'pascalprecht.translate'
+    // 'ionic'
   ]);
+  var translationsEN = {};
+  var translationsVI = {};
 
-  // controller to get profile data
-  app.controller('ProfileController', function($scope, $http){
-    var settings = {
-      method: 'GET',
-      url: 'http://test.fastget.net/api/profiles/',
-      headers:{"authorization": "Basic ZHV5bnhhOkZnMTAwITIzNDU="},
-      async: true,
-      crossDomain: true
-    }
-      $http(settings).then(function(data){
-          $scope.myProfile = data.data;
-      })
-  });
+  app.config(['$translateProvider', function($translateProvider){
+    $translateProvider.translations('en', translationsEN);
+    $translateProvider.translations('vi', translationsVI);
+    $translateProvider.fallbackLanguage('en');
+    $translateProvider.preferredLanguage('en');
+    $translateProvider.useSanitizeValueStrategy('escape');
 
-  // controller for profile Register
-  app.controller('ProfileCreation', function ($scope, $http) {
-    $scope.language = "en"
-    var profileObject={}
-    var settings = {
-      method: 'POST',
-      url: 'http://test.fastget.net/api/profiles/',
-      data: {},
-      headers:{
-        "x-csrftoken": "csrf",
-        "content-type": "application/json",
-        "authorization": "Basic ZmFzdGdldDpGZHNhJDMyMQ=="
-      },
-      async: true,
-      crossDomain: true
-    }
+  }]);
 
-    $scope.createProfile = function (){
-      profileObject = {
-        "username":$scope.phone,
-        "first_name":$scope.first_name,
-      	"last_name":$scope.last_name,
-      	"phone":$scope.phone,
-      	"password":$scope.password,
-      	"address":$scope.address,
-      	"email":$scope.email,
-      	"language":$scope.language
-      };
-      settings.data = Object.assign(settings.data,profileObject);
-      $http(settings).then(function(data){
-          $scope.returnData = data.data;
-          clearFrom();
-          alert('Accout created, Logging in')
-      }, function(response){
-          alert('Can not create account');
-      });
-      function clearFrom(){
-        $scope.phone='';
-        $scope.first_name='';
-      	$scope.last_name='';
-        $scope.phone='';
-      	$scope.password='';
-      	$scope.address='';
-      	$scope.email='';
-      }
-    }
+  app.controller('Ctrl', ['$http', '$translate', '$scope', function ($http, $translate, $scope) {
 
-  });
+    $http.get('http://test.fastget.net/static/languages/vi.json').then(function(data){
+      var res = data.data;
+      console.log(res);
+      translationsVI = Object.assign(translationsVI,res);
 
-  // define directive for Register
+
+    });
+
+    $http.get('http://test.fastget.net/static/languages/en.json').then(function(data){
+      translationsVI = Object.assign(translationsEN,data.data);
+
+    });
+    console.log(translationsVI);
+    console.log(translationsEN);
+    $scope.changeLanguage = function (langKey) {
+      $translate.use(langKey);
+    };
+  }]);
+
 
 })();
